fa684cb49a905fa359c92257f75a8dfdc49bdb0d
arq-gabo <jgabrielguerral@gmail.com>
2019-09-13 20:20:56 +0000

    [FEATURE]Correction_Task_100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    from calculator_1 import *
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) == 4:
-        a = sys.argv[1]
-        b = sys.argv[3]
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         op = sys.argv[2]
         if op == '+':
             print("{} + {} = {}".format(a, b, add(a, b)))
-            exit(0)
         elif op == '-':
             print("{} - {} = {}".format(a, b, sub(a, b)))
-            exit(0)
         elif op == '*':
             print("{} * {} = {}".format(a, b, mul(a, b)))
-            exit(0)
         elif op == '/':
             print("{} / {} = {}".format(a, b, div(a, b)))
-            exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
