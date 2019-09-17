1fcb0628eaa2dcd4f690eadb543392631e4fe788
arq-gabo <jgabrielguerral@gmail.com>
2019-09-17 19:15:41 +0000

    [FEATURE]Correction_If_Task_3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
-        return 
+    if my_list is None:
+        return
     my_list = my_list[::-1]
     for a in my_list:
         print("{:d}".format(int(a)))
