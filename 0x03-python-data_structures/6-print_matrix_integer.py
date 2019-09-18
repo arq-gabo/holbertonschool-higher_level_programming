1b2ad92ac74285756f97d10ccedcbec2041e6794
arq-gabo <jgabrielguerral@gmail.com>
2019-09-18 02:13:08 +0000

    [FEATURE]Correction_Space_task_6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for y in matrix:
+        z = 0
         for x in y:
-            print(x, end=' ')
+            r_space = len(y)
+            z += 1
+            if z != r_space:
+                print(x, end=' ')
+            else:
+                print(x, end='')
         print()
