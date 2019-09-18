19aadfb1904965b62cd5dc476eb00692633dd42a
arq-gabo <jgabrielguerral@gmail.com>
2019-09-18 02:51:15 +0000

    [FIX]Task_8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    lgth = len(sentence)
+    if lgth > 0:
+        first_char = sentence[0]
+    else:
+        None
+    tup = lgth, first_char
+    return tup
