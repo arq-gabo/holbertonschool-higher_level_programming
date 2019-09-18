e24f2a56b1adc6298e1ec56d65d25c15f902b0c6
arq-gabo <jgabrielguerral@gmail.com>
2019-09-18 04:18:48 +0000

    [FEATURE]Correction_Task_10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    cp_my_list = my_list[:]
+    cp_list = my_list[:]
     for i, j in enumerate(my_list):
         if j % 2 == 0:
-            cp_my_list[i] = True
+            cp_list[i] = True
         else:
-            cp_my_list[i] = False
-    return cp_my_list
+            cp_list[i] = False
+    return cp_list
