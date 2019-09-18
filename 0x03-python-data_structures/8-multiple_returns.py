2bf728f97fff5939606cab28b5ef32d7db515995
arq-gabo <jgabrielguerral@gmail.com>
2019-09-18 03:15:30 +0000

    [FEATURE]Task_8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lgth = len(sentence)
-    if lgth > 0:
-        first_char = sentence[0]
-    else:
-        firts_char = None
-    tup = lgth, first_char
-    return(tup)
+    first_char = sentence[0] if len(sentence) > 0 else None
+    return (len(sentence), first_char)
