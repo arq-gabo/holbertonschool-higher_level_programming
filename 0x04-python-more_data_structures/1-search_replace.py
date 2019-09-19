d7047c6bd25017d19a35b26495d9cd2cd5cc52ac
arq-gabo <jgabrielguerral@gmail.com>
2019-09-19 19:47:33 +0000

    [FIX]Task_1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     nw_list = my_list[:]
-    nw_list[search - 1] = replace
+    for i, j in enumerate(nw_list):
+        if j == 2:
+            nw_list[i] = 89
     return nw_list
