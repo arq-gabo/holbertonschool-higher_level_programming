12be5bf33ae336bae265390a8bc77c13f722308d
arq-gabo <jgabrielguerral@gmail.com>
2019-09-19 18:50:51 +0000

    [FEATURE]Correction_task_0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    a = matrix
-    b = matrix
-    c = len(a)
-    d = len(b)
-    nw_matrix = [[a[i][j] * b[i][j] for j in range(c)] for i in range(d)]
-    return(nw_matrix)
+    new_matrix = [[x * x for x in row]for row in matrix]
+    return new_matrix
