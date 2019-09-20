f8afa33d8d651dc57752b8c64126cb6ea91c6aea
arq-gabo <jgabrielguerral@gmail.com>
2019-09-20 02:20:03 +0000

    [FEATURE]Task_10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary:
+        max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
+    else:
         return None
-    max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
-    return(max_key)
+    return max_key
