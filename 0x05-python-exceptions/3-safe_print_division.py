93f6cce4ab64de7e64ef8d43168abeaf13e37f09
arq-gabo <jgabrielguerral@gmail.com>
2019-09-30 22:24:13 +0000

    [FIX]Task_03

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except:
+        result = None
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
