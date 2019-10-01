b3af7f76d0212c009b6f0b3496599d58d83069b7
arq-gabo <jgabrielguerral@gmail.com>
2019-10-01 02:39:55 +0000

    [FEATURE]Correction_Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,3 +9,6 @@ def safe_print_integer_err(value):
     except ValueError as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return False
+    except TypeError as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
