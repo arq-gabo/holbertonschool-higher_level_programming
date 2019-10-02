6f93567f6dc499e5a6fc1be6f6d6d60c4f86415c
arq-gabo <jgabrielguerral@gmail.com>
2019-10-02 01:40:46 +0000

    [FEATURE]Correction_taks_04

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,10 +8,12 @@ class Square:
 
     @property
     def size(self):
+        """size"""
         return self.__size
 
-    @size.setter
+    @size.setter    
     def size(self, value):
+        """"Exception Error"""
         if type(value) is int:
             self.__size = value
             if value < 0:
@@ -20,4 +22,5 @@ class Square:
             raise TypeError("size must be an integer")
 
     def area(self):
+        """Function for find the area square"""
         return(self.__size**2)
