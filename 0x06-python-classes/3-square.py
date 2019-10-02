f40cf253727f8d59df3e5ea58371732d1acc4b06
arq-gabo <jgabrielguerral@gmail.com>
2019-10-02 00:17:47 +0000

    [FEATURE]Corraction_taks_03

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,11 +5,12 @@ class Square:
 
     def __init__(self, size=0):
         """Create Atribute for Class Square whit Init Method"""
+        self.__size = size
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size <= 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
 
     def area(self):
         return(self.__size**2)
