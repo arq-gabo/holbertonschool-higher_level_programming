1dd202efe081f46eeb7417c861a5aba0e144f8a4
arq-gabo <jgabrielguerral@gmail.com>
2019-10-05 00:33:05 +0000

    [FIX]Task_03

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+add_integer module
+"""
+
+def say_my_name(first_name, last_name=""):
+    """Function for concatenate two arguments"""
+
+    if type(first_name) is not str:
+        """Varify type value of the argument"""
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        pass
+
+    print("My name is {} {}".format(first_name, last_name))
