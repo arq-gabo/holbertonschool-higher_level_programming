173af65382fc0e485f25dff05922d72b0444c4e8
arq-gabo <jgabrielguerral@gmail.com>
2019-10-05 01:53:59 +0000

    [FEATURE]Correction_pep8_Task_2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,15 +4,17 @@
 Divide a matrix module
 """
 
+
 def matrix_divided(matrix, div):
     """function for make div operator in a matrix with de div value"""
-    
+
+    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
     for a in range(len(matrix)):
         for b in range(len(matrix[a])):
-            if type(matrix[a][b]) is not int and type(matrix[a][b]) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            else:
+            if type(matrix[a][b]) is int or type(matrix[a][b]) is float:
                 pass
+            else:
+                raise TypeError(err_msg)
 
     if len(matrix[0]) is not len(matrix[1]):
         raise TypeError("Each row of the matrix must have the same size")
@@ -28,5 +30,5 @@ def matrix_divided(matrix, div):
         new_matrix = [[round(a / div, 2) for a in lines]for lines in matrix]
     except ZeroDivisionError:
         raise ZeroDivisionError("division by zero")
-    
+
     return new_matrix
