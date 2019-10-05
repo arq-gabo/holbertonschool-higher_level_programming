0c060eb378d0e0a668f38ec18d31d697ca6c5712
arq-gabo <jgabrielguerral@gmail.com>
2019-10-05 04:08:50 +0000

    [FIX]Task_04

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def text_indentation(text):
+
+    """
+    Indentation of a text separated by special characters
+    """
+
+
+    if type(text) is not str:
+        """Conditional for value is string"""
+        raise TypeError("text must be a string")
+    else:
+        pass
+
+    a = 0
+
+    while a < len(text):
+        if text[a] is "." or text[a] is "?" or text[a] is ":":
+            print(text[a])
+            print()
+            a += 2
+        else:
+            print(text[a], end='')
+            a += 1
