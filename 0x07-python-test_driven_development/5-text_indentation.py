2027e2a409a28bf7afc02262b35f0e3a39fd8aff
arq-gabo <jgabrielguerral@gmail.com>
2019-10-11 00:29:54 +0000

    [FEATURE]Task_5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,7 @@ def text_indentation(text):
     """
 
 
-    if type(text) is not str:
+    if type(text) is not str or text is None or len(text) < 0:
         """Conditional for value is string"""
         raise TypeError("text must be a string")
     else:
