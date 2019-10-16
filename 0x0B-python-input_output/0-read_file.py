0d120db942d38526c655e4bee99b0f38f0cb2608
arq-gabo <jgabrielguerral@gmail.com>
2019-10-16 19:11:45 +0000

    [FEATURE]Add_Comments_task_00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """function por read the file .txt"""
     with open(filename, "r", encoding="utf-8") as read_file:
         print(read_file.read())
