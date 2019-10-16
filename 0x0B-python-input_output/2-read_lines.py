97ee16a469b103dd6221d196fb6f198b63f4b6f7
arq-gabo <jgabrielguerral@gmail.com>
2019-10-16 20:32:31 +0000

    [FEATURE]task_02

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    """Function for print N number of lines in a txt file"""
     with open(filename, "r", encoding="utf-8") as f:
         for i, lines in enumerate(f):
             if i < nb_lines or nb_lines <= 0:
