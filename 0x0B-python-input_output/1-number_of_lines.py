4639d5602f2bd97baf39c2ca6b57876f014b94f1
arq-gabo <jgabrielguerral@gmail.com>
2019-10-16 23:13:44 +0000

    [FEATURE]Correction

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     """Function for count number lines"""
-    with open(filename) as f:
+    with open(filename, "r", encoding="utf-8") as f:
         for i, j in enumerate(f):
             pass
     return i + 1
