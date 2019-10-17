3a50dc9ef32277dad4e55d5cc699300068023b40
arq-gabo <jgabrielguerral@gmail.com>
2019-10-17 16:59:50 +0000

    [FEATURE]Correction_pep8

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,7 +3,6 @@ class BaseGeometry:
     """Class BaseGeometry for a method with public instance"""
     pass
 
-
     def area(self):
         """method for area public instance"""
         raise Exception("area() is not implemented")
