9fc1f6e8118057f5868a23862315470f5b27825a
arq-gabo <jgabrielguerral@gmail.com>
2019-10-17 18:57:58 +0000

    [FEATURE]Correction_import_method_rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
+"""Module"""
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-
-class Rectangle(BaseGeometry):
-    """Class that inherits methods of class BaseGeometry"""
-    def __init__(self, width, height):
-        """Method for validate values of with and height"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """method for calculate the reactangule area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """return string message"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
