96af8185ad3a8fe739c8ad42ab9ab413c72a1064
arq-gabo <jgabrielguerral@gmail.com>
2019-10-17 04:13:22 +0000

    [FEATURE]Task_11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,10 +8,10 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Init method initialization"""
-        self.first_name = firts_name
+        self.first_name = first_name
         self.last_name = last_name
-        self.age_name = age
+        self.age = age
 
     def to_json(self):
         """Method to json"""
-        return obj.__dict__
+        return self.__dict__
