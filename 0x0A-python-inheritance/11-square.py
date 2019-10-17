6e959fb85a5bc255af0deb229c368250c4390a27
arq-gabo <jgabrielguerral@gmail.com>
2019-10-17 19:12:19 +0000

    [FEATURE]Correction_insistance_validate

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,13 +5,12 @@
 Rectangle = __import__("9-rectangle").Rectangle
 
 
-
-
 class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
         """Super init for change value width an height for size"""
         self.__size = size
+        self.integer_validator("size", size)
         super().__init__(self.__size, self.__size)
 
     def __str__(self):
