133c7eeee12a844f1c840fc9ac6e210ce61af8ca
arq-gabo <jgabrielguerral@gmail.com>
2019-10-17 17:11:17 +0000

    [FEATURE]Integer_validator_instace_size

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -26,4 +26,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """Muper init for change value width an height for size"""
         self.__size = size
+        self.integer_validator("size", size)
         super().__init__(self.__size, self.__size)
