f536185a7b08737a5dfd1caec4ec0fc9d94ccbfb
arq-gabo <jgabrielguerral@gmail.com>
2019-10-17 04:08:50 +0000

    [FIX]task_11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+"""Module of the 11-student"""
+
+
+class Student:
+    """Create class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Init method initialization"""
+        self.first_name = firts_name
+        self.last_name = last_name
+        self.age_name = age
+
+    def to_json(self):
+        """Method to json"""
+        return obj.__dict__
