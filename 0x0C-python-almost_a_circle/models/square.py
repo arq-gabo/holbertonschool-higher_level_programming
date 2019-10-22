62b7d5be47213459a409c1dbf4528e2fc7c37cf0
arq-gabo <jgabrielguerral@gmail.com>
2019-10-22 03:22:12 +0000

    [FEATURE]Update_square_file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,7 @@ class Square(Rectangle):
     def size(self, size):
         """Method Setter for validate size value"""
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif size <= 0:
             raise ValueError("width must be > 0")
         else:
@@ -33,3 +33,25 @@ class Square(Rectangle):
         """Method fot anulate __str__"""
         return("[Square] ({}) {}/{} - {}".format(self.id,
                 self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """Method for update class square with *args **kwargs"""
+        new_list = ['id', 'size', 'x', 'y']
+        new_dict = {'id': 'id', 'width': '_Rectangle__width', 'height'
+                    : '_Rectangle__height', 'x': '_Rectangle__x', 'y':
+                    '_Rectangle__y'}
+
+        for num, val in enumerate(args):
+            if num != 1:
+                self.__dict__[new_dict[new_list[num]]] = val
+            else:
+                self.height = val
+                self.width = val
+
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                if key != "size":
+                    self.__dict__[new_dict[key]] = value
+                else:
+                    self.height = value
+                    self.width = value
