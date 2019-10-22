c14efe121165feb3597039c47bdf358d154b23ce
arq-gabo <jgabrielguerral@gmail.com>
2019-10-22 20:18:18 +0000

    [FEATURE]Correction_super_method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,11 +11,11 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor Class Method Init"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     def area(self):
         """Method for calculate area"""
