7ee8a0d0062f90e0905a747dcfcc05c99f597445
arq-gabo <jgabrielguerral@gmail.com>
2019-10-23 00:51:26 +0000

    [FIX]Upload_rectangle_test_file

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""Module test_rectangle"""
+
+
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    """class to purchase the functionality of the rectangle file"""
+
+    def setUp(self):
+        """"""
