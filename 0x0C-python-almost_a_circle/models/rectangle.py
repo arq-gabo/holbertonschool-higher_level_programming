b488f9775d38ccbda7d88bd8a7f3bb9f47797dd1
arq-gabo <jgabrielguerral@gmail.com>
2019-10-24 00:05:50 +0000

    [FEATURE]Correction_update_method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,8 +31,8 @@ class Rectangle(Base):
         """Method for assigns an argument to each attribute"""
         new_list = ['id', 'width', 'height', 'x', 'y']
         if args:
-            for i for range(len(args)):
-                setattr(self, new_list[i], args[i])
+            for j in range(len(args)):
+                setattr(self, new_list[j], args[j])
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
