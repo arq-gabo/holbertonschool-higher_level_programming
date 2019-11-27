2677ebb438ba4fbe60a95839ada1865d8361f813
arq-gabo <jgabrielguerral>
2019-11-27 06:04:03 +0000

    [FEATURE]Task_09

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- script for list all cities contained in the database
 SELECT cities.id, cities.name, states.name FROM cities
-       INNER JOIN states ON cities.states.id = states.id
+       INNER JOIN states ON cities.states_id = states.id
        ORDER BY cities.id ASC;
