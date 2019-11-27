9e2ee68a1a217dffa1b27b7c85a188e306d24108
arq-gabo <jgabrielguerral>
2019-11-27 04:43:10 +0000

    [FIX]Task_08

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Script for show list with contain the cities of California in the database
-SELECT id, name FROM cities WHERE state_id =
+SELECT id, name FROM cities WHERE state_id = ANY
         (SELECT id FROM states WHERE name = 'California') ORDER BY id DESC;
