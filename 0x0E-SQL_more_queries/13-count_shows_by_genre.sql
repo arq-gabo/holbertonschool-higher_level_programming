d0e8bd3b95156e8b7f998a421abc8377413a9d75
arq-gabo <jgabrielguerral>
2019-11-28 03:53:45 +0000

    [FEATURE]Task_13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Script for Show for enumerate all generes of hbtn_0d_tvshows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+       FROM tv_show_genres
+       INNER JOIN tv_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       GROUP BY tv_genres.name
+       ORDER BY number_of_shows DESC
