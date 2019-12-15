c354a41e2e853fe01711f562e15f0c592c324b06
arq-gabo <jgabrielguerral>
2019-12-15 04:48:52 +0000

    [FIX]Task_01

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     myCursor.execute("SELECT * FROM states \
                       WHERE name LIKE 'N%' \
-                      ORDER BY id ASC")
+                      ORDER BY states.id ASC")
 
     valuesDb = myCursor.fetchall()
 
