557a510665af3e16653a703ec93e2965ecfef79e
arq-gabo <jgabrielguerral>
2019-12-15 04:17:02 +0000

    [FETURE]Task_01

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Module"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """connection with database"""
+    data_base = MySQLdb.connect(user=sys.argv[1],
+                                passwd=sys.argv[2],
+                                db=sys.argv[3],
+                                host='localhost',
+                                port=3306)
+
+    myCursor = data_base.cursor()
+
+    myCursor.execute("""SELECT * FROM states 
+                      WHERE name LIKE 'N%'
+                      ORDER BY id ASC""")
+
+    valuesDb = myCursor.fetchall()  
+
+    for val_db in valuesDb:
+        print(val_db)
+
+    myCursor.close()
+    data_base.close()
