f61152df8ae8d26485857e8e3fbb3ea0b2e27643
arq-gabo <jgabrielguerral>
2019-12-16 04:46:49 +0000

    [FIX]Task_05

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     myCursor.execute("SELECT cities.name\
                       FROM cities WHERE cities.state_id =\
                       (SELECT id FROM states\
-                      WHERE name = %s) ORDER BY cities.id ASC;", (sys.argv[4],))
+                      WHERE name = %s)\
+                      ORDER BY cities.id ASC;", (sys.argv[4], ))
 
     values_db = myCursor.fetchall()
 
