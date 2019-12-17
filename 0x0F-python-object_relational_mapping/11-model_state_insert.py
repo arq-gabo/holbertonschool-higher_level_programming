7a2b665cc96fc87bd4b4d4ea9a89e3ce664a504d
arq-gabo <jgabrielguerral@gmail.com>
2019-12-17 13:30:13 -0500

    [FEATURE]Task_11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""MODULE"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    DBSession = sessionmaker(bind=engine)
+
+    session = DBSession()
+
+    add_state = State(name='Louisiana')
+
+    session.add(add_state)
+
+    session.commit()
