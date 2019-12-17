7bc00513fced2650b1a56ea6de53b4088c0631a8
arq-gabo <jgabrielguerral@gmail.com>
2019-12-17 00:37:01 -0500

    [FEATURE]Task_10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""MODULE"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    DBSession = sessionmaker(bind=engine)
+
+    session = DBSession()
+
+    state_argv = session.query(State).filter_by(name=sys.argv[4]).first()
+
+    if state_argv:
+        print("{}".format(state_argv.id))
+    else:
+        print("Not found")
