6bfac76bc795f325cbbaa7ac8a4b0d6f2c181505
arq-gabo <jgabrielguerral@gmail.com>
2020-01-07 03:17:36 +0000

    [FIX]Task_00

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Script of bash with show the size of the body of the response
-#curl -sI "$@" | awk '/Content-Length/{gsub("\\r", ""); print $2}'
-curl -sI "$1" | grep "^Content-Length" | cut -d ' ' --fields=2
+curl -sI "$@" | awk '/Content-Length/{gsub("\\r", ""); print $2}'
