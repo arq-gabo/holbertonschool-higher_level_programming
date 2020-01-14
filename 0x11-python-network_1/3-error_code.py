14f481ce0d0acf18826c8d4ebd51a411a280416a
arq-gabo <jgabrielguerral@gmail.com>
2020-01-13 21:48:59 -0500

    [FEATURE]Task_03

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module for show the request
+"""
+
+import sys
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+
+if __name__ == "__main__":
+
+
+    url = sys.argv[1]
+
+    try:
+        req = Request(url)
+        with urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.code)
