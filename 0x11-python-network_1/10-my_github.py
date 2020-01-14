ed463edaf0ebe95a2e34e10622e0921c717d49b1
arq-gabo <jgabrielguerral@gmail.com>
2020-01-14 18:19:29 -0500

    [FIX]Taks_10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     users = sys.argv[1]
     passw = sys.argv[2]
-    url = "https://api.github.com/users/{}".format(users)
+    url = "https://api.github.com/user"
     r = requests.get(url, auth=(users, passw))
     d = r.json()
     print(d.get("id"))
