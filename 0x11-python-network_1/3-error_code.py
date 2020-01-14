032bd41113dc9aa5653a55ad01b698f04a4e2250
arq-gabo <jgabrielguerral@gmail.com>
2020-01-14 02:58:26 +0000

    [FIX]Task_03

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,10 +7,9 @@ Module for show the request
 import sys
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
-
+import urllib.error
 
 if __name__ == "__main__":
-
 
     url = sys.argv[1]
 
@@ -18,5 +17,6 @@ if __name__ == "__main__":
         req = Request(url)
         with urlopen(req) as response:
             print(response.read().decode('utf-8'))
+
     except urllib.error.HTTPError as e:
-        print(e.code)
+        print("Error code: {}".format(e.code))
