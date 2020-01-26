fd6ac8384a2c5a9e453593d62bf40e5cf86d815a
arq-gabo <jgabrielguerral@gmail.com>
2020-01-25 22:37:08 -0500

    [FIX]Task_11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,21 +7,20 @@ import sys
 import requests
 
 
-
 if __name__ == "__main__":
+
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1]
-    )
-    r = requests.get(url)
-    counter = 0
+        sys.argv[2], sys.argv[1])
+
+    a = requests.get(url)
+    count = 0
     try:
-        json_data = r.json()
+        json_data = a.json()
         for data in json_data:
-            if counter < 10:
+            if count < 10:
                 print("{}: {}".format(
                     data.get('sha'),
-                    data['commit']['author'].get('name'))
-                  )
-                counter = counter + 1
+                    data['commit']['author'].get('name')))
+                count = count + 1
     except ValueError:
         print("Not a valid JSON")
