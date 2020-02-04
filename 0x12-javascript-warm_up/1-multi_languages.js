592d4c8364d9625f9f711321a2535076cbdd6bdb
arq-gabo <jgabrielguerral@gmail.com>
2020-02-04 21:45:59 +0000

    [FIX]add point

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const txt1 = 'C is fun';
-const txt2 = 'Python is cool'
-const txt3 = 'Javascript is amazing'
+const txt2 = 'Python is cool';
+const txt3 = 'Javascript is amazing';
 console.log(txt1);
 console.log(txt2);
 console.log(txt3);
