af1d6dabd3a5301aadbb80df17c1e5522dc744e2
arq-gabo <jgabrielguerral@gmail.com>
2020-02-11 05:37:19 +0000

    [FIX]Task_08

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let arr = [];
-  for (let a = list.lenght - 1; a >= 0; a--){
+  const arr = [];
+  for (let a = list.length - 1; a >= 0; a--) {
     arr.push(list[a]);
   }
   return (arr);
